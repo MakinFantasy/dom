dfedf0ec5d9e5d3dc1c07a54a9349b2a21c6059d
MakinFantasy <beingp0z1t1v3@gmail.com>
2023-02-07 20:27:46 +0300

    fixed eslintignore

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 // TODO: write code here
 // comment this to pass build
-import Playground from './playground/Playground';
+import Playground from "./playground/Playground";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const playingArea = new Playground(document.querySelector('.playground'));
+document.addEventListener("DOMContentLoaded", () => {
+  const playingArea = new Playground(document.querySelector(".playground"));
 
   setInterval(playingArea.randomAppear, 1000);
-})
+});
